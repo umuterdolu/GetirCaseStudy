4ad5b9ca6932b3ab88427bee57427fa4ff026ee1
umutismeterdolu <Umut/3547>
2022-05-06 16:20:02 +0300

    Initial Commit

---
+++ src/main/java/com/readingisgood/ReadingIsGoodApp.java
@@ -0,0 +1,14 @@
+package com.readingisgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Umut Ismet Erdolu
+ */
+@SpringBootApplication
+public class ReadingIsGoodApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingIsGoodApp.class, args);
+    }
+}
